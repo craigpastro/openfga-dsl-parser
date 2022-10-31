c5eab1354e4866134733081c1ecd4669bd6006cd
Craig Pastro <craig.pastro@okta.com>
2022-10-31 14:58:01 -0700

    Add parse function (#4)

--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -6,11 +6,16 @@ import (
 	"os"
 
 	parser "github.com/craigpastro/openfga-dsl-parser"
+	pb "go.buf.build/openfga/go/openfga/api/openfga/v1"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
 	var filename string
 	flag.StringVar(&filename, "f", "", "specify a file to read")
+
+	var prettyPrint bool
+	flag.BoolVar(&prettyPrint, "p", false, "pretty print the output")
 	flag.Parse()
 
 	var data string
@@ -21,8 +26,17 @@ func main() {
 		}
 		data = string(bytes)
 	} else {
-		data = os.Args[1]
+		data = os.Args[len(os.Args)-1]
 	}
 
-	fmt.Println(parser.MustParse(data))
+	output := &pb.AuthorizationModel{
+		SchemaVersion:   "1.0",
+		TypeDefinitions: parser.MustParse(data),
+	}
+
+	if prettyPrint {
+		fmt.Println(protojson.MarshalOptions{Multiline: true}.Format(output))
+	} else {
+		fmt.Println(protojson.MarshalOptions{}.Format(output))
+	}
 }
